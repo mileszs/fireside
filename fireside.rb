16569475372423354109222eee9d07644aae49b4
Miles Z. Sterrett <miles@mileszs.com>
2012-11-14 19:52:38 -0500

    Add Post model, using Datamapper

--- fireside.rb
+++ fireside.rb
@@ -4,10 +4,24 @@ require 'dm-validations'
 require 'dm-migrations'
 require 'camping'
 
+# If you want the logs displayed you have to do this before the call to setup
+DataMapper::Logger.new($stdout, :debug)
+
+# An in-memory Sqlite3 connection:
+DataMapper.setup(:default, 'sqlite::memory:')
+
 Camping.goes :Fireside
 
 module Fireside::Models
-  class Post < Base; belongs_to :user; end
+  class Post
+    include DataMapper::Resource
+
+     property :id,         Serial    # An auto-increment integer key
+     property :title,      String    # A varchar type string, for short strings
+     property :body,       Text      # A text block, for longer string data.
+     property :created_at, DateTime  # A DateTime, for any date you might like.
+  end
+
   class Comment < Base; belongs_to :user; end
   class User < Base; end
 end
