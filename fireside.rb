0657bc5a21c1895613379a2d18756bef0f56a88c
David Christiansen <dave@developertown.com>
2012-11-14 19:43:35 -0500

    Start up

---
+++ fireside.rb
@@ -0,0 +1,36 @@
+require 'camping'
+
+Camping.goes :Fireside
+
+module Fireside::Models
+  class Post < Base; belongs_to :user; end
+  class Comment < Base; belongs_to :user; end
+  class User < Base; end
+end
+
+module Fireside::Controllers
+  class Index
+    def get
+      @posts = Post.find :all
+      render :index
+    end
+  end
+end
+
+module Fireside::Views
+  def layout
+    html do
+      head { title "My Blog" }
+      body do
+        h1 "My Blog"
+        self << yield
+      end
+    end
+  end
+
+  def index
+    @posts.each do |post|
+      h1 post.title
+    end
+  end
+end
